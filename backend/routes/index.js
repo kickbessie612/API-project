09b6d43bcb3829658b7afa103360d0c0246c0134
Yuan Wang <yuanwang612@gmail.com>
2022-10-29 13:15:36 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,8 +7,12 @@ router.get('/api/csrf/restore', (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie('XSRF-TOKEN', csrfToken);
   res.status(200).json({
-    'XSRF-Token': crsfToken
+    'XSRF-Token': csrfToken
   });
 });
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 module.exports = router;
