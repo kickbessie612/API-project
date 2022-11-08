42c89081e27ddc71c5fac29588f97c73bbd1d4b9
Yuan Wang <yuanwang612@gmail.com>
2022-11-08 15:09:26 -0800

    change return to await

--- backend/db/seeders/20221103045121-demo-user.js
+++ backend/db/seeders/20221103045121-demo-user.js
@@ -30,7 +30,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     const Op = Sequelize.Op;
 
-    return queryInterface.bulkDelete(
+    await queryInterface.bulkDelete(
       'Users',
       { username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] } },
       {}
